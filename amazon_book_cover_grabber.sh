8b3650734a1a4f9cb3d84c3803979df3d281b88d
drewmarsh <drewmmisc@gmail.com>
2024-09-11 13:33:20 -0400

    Add user-configurable options

--- amazon_book_cover_grabber.sh
+++ amazon_book_cover_grabber.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# User-configurable options
+ask_run_again=true
+ask_save=true
+default_save_dir="$HOME/Downloads"  # Default save-to-disk directory
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -53,44 +58,79 @@ open_url() {
     esac
 }
 
-# Function to download and save image
-download_image() {
+# Function to save image to disk
+save_image() {
     local url="$1"
     local asin="$2"
-    local download_dir
+    local save_dir
 
-    echo ""
-    printf "Enter the directory where you want to save the image: "
-    read -r download_dir
+    if [ -z "$default_save_dir" ] || [ ! -d "$default_save_dir" ]; then
+        # Default save directory is invalid or empty, skip to custom directory input
+        echo ""
+        printf "Enter the custom directory where you want to save the image: "
+        read -r save_dir
+        # Expand ~ to home directory if used
+        save_dir=$(eval echo "$save_dir")
+    elif [ "$ask_save" = false ]; then
+        save_dir="$default_save_dir"
+    else
+        echo ""
+        echo "How would you like to save the image?"
+        echo ""
+        echo "1. Use the default save directory ($default_save_dir) as defined in the script"
+        echo "2. Enter a custom directory"
+        echo ""
+        printf "Enter your choice here: "
+        
+        while true; do
+            read -r choice
+            case $choice in
+                1)
+                    save_dir="$default_save_dir"
+                    break
+                    ;;
+                2)
+                    echo ""
+                    printf "Enter the custom directory where you want to save the image: "
+                    read -r save_dir
+                    # Expand ~ to home directory if used
+                    save_dir=$(eval echo "$save_dir")
+                    break
+                    ;;
+                *)
+                    echo ""
+                    printf "Invalid choice. Please enter 1 or 2: "
+                    ;;
+            esac
+        done
+    fi
+
     echo ""  # Add a blank line after user input
 
-    # Expand ~ to home directory if used
-    download_dir=$(eval echo "$download_dir")
-
-    if [ ! -d "$download_dir" ]; then
+    if [ ! -d "$save_dir" ]; then
         echo "The specified directory does not exist. Creating it now."
-        mkdir -p "$download_dir"
+        mkdir -p "$save_dir"
         if [ $? -ne 0 ]; then
             echo "Failed to create directory. Please check permissions and try again."
             return 1
         fi
     fi
 
-    local filename="${download_dir}/${asin}_cover.jpg"
+    local filename="${save_dir}/${asin}_cover.jpg"
 
     if command -v curl >/dev/null 2>&1; then
         curl -o "$filename" "$url"
     elif command -v wget >/dev/null 2>&1; then
         wget -O "$filename" "$url"
     else
-        echo "Error: Neither curl nor wget is available. Unable to download the image."
+        echo "Error: Neither curl nor wget is available. Unable to save the image."
         return 1
     fi
 
     if [ $? -eq 0 ]; then
         echo "Image saved as: $filename"
     else
-        echo "Failed to download the image."
+        echo "Failed to save the image."
         return 1
     fi
 }
@@ -117,23 +157,27 @@ process_input() {
     echo "in the default browser"
     open_url "$cover_url"
 
-    echo ""
-    printf "Would you like to save this file to disk? ("
-    print_color "$GREEN" "Y"
-    printf "/"
-    print_color "$RED" "N"
-    printf "): "
-    read -r save_answer
-    case $save_answer in
-        [Yy]* ) 
-            download_image "$cover_url" "$asin"
-            ;;
-    esac
+    if [ "$ask_save" = true ]; then
+        echo ""
+        printf "Would you like to save this file to disk? ("
+        print_color "$GREEN" "Y"
+        printf "/"
+        print_color "$RED" "N"
+        printf "): "
+        read -r save_answer
+        case $save_answer in
+            [Yy]* ) 
+                save_image "$cover_url" "$asin"
+                ;;
+        esac
+    fi
 }
 
 # Main script
-run_script() {
-    while true; do
+main() {
+    local input
+    
+    if [ $# -eq 0 ]; then
         echo ""
         printf "Enter the "
         print_color "$YELLOW" "Amazon book URL"
@@ -147,31 +191,26 @@ run_script() {
         if [ "$input" = "q" ]; then
             return 0
         fi
+    else
+        input="$1"
+    fi
 
-        if process_input "$input"; then
-            break
-        fi
-    done
+    process_input "$input"
 
-    echo ""
-    printf "Do you want to run the script again? ("
-    print_color "$GREEN" "y"
-    printf "/"
-    print_color "$RED" "n"
-    printf "): "
-    read -r answer
-    case $answer in
-        [Yy]* ) return 1 ;;
-        * ) return 0 ;;
-    esac
+    if [ "$ask_run_again" = true ]; then
+        echo ""
+        printf "Do you want to run the script again? ("
+        print_color "$GREEN" "Y"
+        printf "/"
+        print_color "$RED" "N"
+        printf "): "
+        read -r answer
+        case $answer in
+            [Yy]* ) 
+                main
+                ;;
+        esac
+    fi
 }
 
-if [ $# -eq 0 ]; then
-    while true; do
-        if run_script; then
-            break
-        fi
-    done
-else
-    process_input "$1"
-fi
+main "$@"
